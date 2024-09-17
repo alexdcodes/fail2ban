eaaecf0a12f630a264fe39ab4b0a2a31cc8a2fe2
Alex Diker <23444429+alexdcodes@users.noreply.github.com>
2024-09-17 00:43:25 -0400

    macosx check fail2ban startup
    checking if it is enabled and working

---
+++ macosxc_configurations/fail2ban.sh
@@ -0,0 +1,9 @@
+#shortcut python3 macosx
+# -----------------------------------------------------------------------------------
+
+
+sudo python3 /Library/Frameworks/Python.framework/Versions/3.8/bin/fail2ban-server
+sudo python3 /Library/Frameworks/Python.framework/Versions/3.8/bin/fail2ban-client status
+
+# -----------------------------------------------------------------------------------
+
